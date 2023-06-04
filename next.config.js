93b9be6f9fc37cca95d3c10b2efc41f535e84c66
manjit <you@example.com>
2023-06-03 22:21:30 -0700

    final1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+        
+        "gkhcdfdlkhevjmpgobxz.supabase.co"
+    ],
+  },
+};
 
 module.exports = nextConfig
